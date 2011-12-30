42eaeedc4a8b63c0f55f4912bafcacc28a966315
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2011-12-30 12:43:45 -0200

    doesn't work yet, WIP

--- erlang/Main.scala
+++ erlang/Main.scala
@@ -30,64 +30,61 @@ object Main {
   }
   
   def main(args: Array[String]): Unit = {
-    for(i <- 0 to 999) {
+    var total:Long=0;
+    val iters=100000
+    var result = 0
+    for(i <- 1 to iters) {
       val start_time = System.nanoTime();
-      val terms = make_terms(args, List());
+      val terms = make_terms(args);
       val expr = make_expr(terms, List());
-      val result = calculate(expr)
+      result = calculate(expr)
       val end_time = System.nanoTime();
-      println("Scala\ttime = " + (end_time - start_time)/1000 + "\tresult = " + result);
+      total=total+(end_time - start_time)/1000;
+      //println("Scala\ttime = " + (end_time - start_time)/1000 + "\tresult = " + result);
     }
+    println("result="+result);
+    println ("avg ="+ (total/iters));
   }
 
-  def make_terms(args: Array[String], terms: List[Term]): List[Term] = {
-    val arg = args.head;
-    val next = arg match {
-      case "+" => new Operator(Add)
-      case "-" => new Operator(Subtract)
-      case "x" => new Operator(Multiply)
-      case "/" => new Operator(Divide)
-      case n => new Number(arg.toInt)
-    }
-    if(args.tail.size == 0) {
-      (next :: terms).reverse
-    }
-    else {
-      make_terms(args.tail, next :: terms)
-    }
-  }
+  
+  def make_terms(args: Seq[String]): List[Term] = 
+    if (args.isEmpty) {
+      Nil
+    } else {
+      val arg = args.head;
+      val next = arg match {
+        case "+" => new Operator(Add)
+        case "-" => new Operator(Subtract)
+        case "x" => new Operator(Multiply)
+        case "/" => new Operator(Divide)
+        case n => new Number(arg.toInt)
+      }
+    next :: make_terms(args.tail)
+   }
 
   def make_expr(terms: List[Term], stack: List[Expression]): Expression = {
-    val top =terms.head
-    top match {
-      case x: Operator =>
-        if(terms.tail.isEmpty) {
-          new ComplexExpr(x, stack(1), stack(0))
+    val head::rest=terms
+    head match {
+      case x: Operator => {
+          val stop::st0::st1::srest=stack
+          rest match {
+            case Nil => new ComplexExpr(x, st1, st0)
+            case _ =>  make_expr(rest, new ComplexExpr(x, st1, st0) :: srest)
+          }
         }
-        else {
-          make_expr(terms.tail, new ComplexExpr(x, stack(1), stack(0)) :: stack.drop(2))
-        }
-      case x: Number =>
-        make_expr(terms.tail, new SimpleExpr(x.asInstanceOf[Number].value) :: stack)
+      case Number(value) => make_expr(terms.tail, new SimpleExpr(value) :: stack)
     }
   }
 
   def calculate(expr: Expression): Int = {
-    if(expr.isInstanceOf[SimpleExpr]) {
-      expr.asInstanceOf[SimpleExpr].value
-    }
-    else {
-      val thisexpr: ComplexExpr = expr.asInstanceOf[ComplexExpr]
-      val thisop: Op = thisexpr.op.value
-      thisop match {
-        case Add =>
-          calculate(thisexpr.op1) + calculate(thisexpr.op2)
-        case Subtract =>
-          calculate(thisexpr.op1) - calculate(thisexpr.op2)
-        case Multiply =>
-          calculate(thisexpr.op1) * calculate(thisexpr.op2)
-        case Divide =>
-          calculate(thisexpr.op1) / calculate(thisexpr.op2)
+    expr match {
+      case SimpleExpr(value) => value;
+      case ComplexExpr(op,op1,op2) =>
+      op match {
+        case Add =>  calculate(op1) + calculate(op2)
+        case Subtract => calculate(op1) - calculate(op2)
+        case Multiply => calculate(op1) * calculate(op2)
+        case Divide =>   calculate(op1) / calculate(op2)
       }
     }
   }
